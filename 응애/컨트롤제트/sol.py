442edaca6010e37bbb598dc269b9c375ddc6dc96
kiku99 <elsd0326@gmail.com>
2023-05-16 16:53:57 +0900

    add

---
+++ 응애/컨트롤제트/sol.py
@@ -0,0 +1,11 @@
+def solution(s):
+    
+    answer = []
+    
+    for i in s.split():
+        if i == 'Z':
+            answer.pop()
+            continue
+        answer.append(int(i))
+        
+    return sum(answer)
